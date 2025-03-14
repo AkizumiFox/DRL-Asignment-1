a923cd9035e586fdfc1321fd18f2acd193872473
AkizumiFox <akizumi.fox@gmail.com>
2025-03-14 09:57:59 +0800

    dqrn0132

--- student_agent.py
+++ student_agent.py
@@ -234,7 +234,7 @@ def get_action(obs):
     
     # Get action from agent, passing current hidden state
     action, get_action.hidden_state = get_action.agent.act(state, get_action.hidden_state)
-    action_map = {0: 1, 1: 0, 2: 3, 3: 2, 4: 4, 5: 5}
+    action_map = {0: 0, 1: 1, 2: 3, 3: 2, 4: 4, 5: 5}
     action = action_map[action]
     
     # Store for next call
